0e5989da9f6a3f134402a6471bd579927129849c
katariyajatin <jatinkatariya2005@gmail.com>
2025-03-25 08:37:59 +0530

    local project

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Col, Container, Row } from 'react-bootstrap'
+
+
+
+const Home = () => {
+  function App() {
+    const [product, setProducts] = useState(null);
+
+    useEffect(() => {
+      axios.get('https://dummyjson.com/products')
+        .then(response => {
+          setProducts(response.data.products);
+        })
+        .catch(error => {
+          console.error('Error fetching the products', error);
+        });
+    }, []);
+  }
+  return (
+    <Container className="mt-4">
+      <h1>Our Products</h1>
+      <Row>
+        {/* {
+          product != null ?
+            product.map((v, i) => (
+              <Col md={4} key={i} className="mb-4">
+                <Card>
+                  <Card.Img variant="top" src={v.thumbnail} />
+                  <Card.Body>
+                    <Card.Title>{v.title}</Card.Title>
+                    <Card.Text>{v.description}</Card.Text>
+                    <Card.Text><strong>${v.price}</strong></Card.Text>
+                    <Button variant="primary">Add to Cart</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))
+            :
+            <p>Loading products...</p>
+        } */}
+      </Row>
+    </Container>
+  );
+
+}
+
+export default Home
